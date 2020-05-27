8d92d821ca516264571b4a3d03516788c6f1de08
Steve Holland <sdh4@iastate.edu>
2020-05-27 15:01:46 -0500

    Initial commit of NDEed-tiny-editor

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
-# el-tiny-editor
+# NDEed-tiny-editor: Editor for the NDE-ed.org website 
+# forked from https://github.com/bmaranville/el-tiny-editor
 
-To run, install node and npm and `npm install`, then `npm start`
+To build:
+npm install
+patch -p0 <tinymce_importcss_ceFalseOverride.patch
+cp node_modules/tinymce/plugins/importcss/plugin.js node_modules/tinymce/plugins/importcss/plugin.min.js
+npm install
+npm start
+
+To run, install node and npm and `npm install`, then `npm start` from
+this directory
+
+on Linux, to install systemwide: sudo npm install -g electron --unsafe-perm=true --allow-root
+
+Can then run from central path with "electron <this_directory> [ file_to_open.html ]"
+
--- renderer.js
+++ renderer.js
@@ -16,9 +16,11 @@ function save() {
   if (filename == null) {
     saveas();
   } else {
-    var output = tinymce.editors[0].getContent({format: 'raw'});
+    var output = tinymce.editors[0].getContent(); // {format: 'raw'});
     var fullpath = path.join(working_directory, filename);
-    fs.writeFile(filename, beautify(output, {indent_size: 2}), (err) => {
+    console.log("saving file:");  
+    console.log(fullpath);  
+    fs.writeFile(fullpath, beautify(output, {indent_size: 2}), (err) => {
       if (err) throw err;
       console.log('It\'s saved!');
     });
@@ -28,33 +30,46 @@ function save() {
 function saveas() {
   var options = {
     filters: [
-      {name: 'html', extensions: ['htm', 'html']},
+      {name: 'xhtml', extensions: ['xhtml']},
       {name: 'All Files', extensions: ['*']}
     ],
     defaultPath: working_directory
   };
-  dialog.showSaveDialog(options, function(n) {
-    if (n == null) {
+    dialog.showSaveDialog(options).then((n) => {
+    if (n.canceled) {
       return; // cancelled
     }
     else {
-      change_working_directory(path.dirname(n));
-      filename = path.basename(n); 
+      console.log("Save dialog returned");
+      console.log(n);
+      change_working_directory(path.dirname(n.filePath));
+      filename = path.basename(n.filePath); 
       save();
     }
   });
 }
 
 function load() {
-  dialog.showOpenDialog({properties: ["openFile"], defaultPath: working_directory}, function(fn) {
-    fs.readFile(fn[0], 'utf8', (err, data) => {
-      if (err) throw err;
-      tinymce.editors[0].setContent(data);
-    });
+  console.log('calling load()');
+    dialog.showOpenDialog({properties: ["openFile"], defaultPath: working_directory}).then((fn) => {
+	if (fn.canceled) return;
+
+      change_working_directory(path.dirname(fn.filePaths[0]));
+
+      filename=path.basename(fn.filePaths[0]);
+      console.log('calling readFile()');
+      console.log(fn.filePaths[0]);
+      fs.readFile(fn.filePaths[0], 'utf8', (err, data) => {
+        if (err) throw err;
+        tinymce.editors[0].setContent(data);
+      });
   });
 }
 
 function change_working_directory(new_path) {
+  //if (new_path=="") {
+  //  new_path=".";
+  //}
   working_directory = new_path;
   if (tinymce.activeEditor) {
     var doc = tinymce.activeEditor.getDoc(),
@@ -69,9 +84,9 @@ function change_working_directory(new_path) {
 }
 
 function choose_working_directory() {
-  dialog.showOpenDialog({properties: ["openDirectory"], defaultPath: working_directory}, function(fn) {
-    if (fn) {
-      change_working_directory(fn[0]);
+  dialog.showOpenDialog({properties: ["openDirectory"], defaultPath: working_directory}).then((fn) => {
+    if (!fn.cancelled) {
+      change_working_directory(fn.filePaths[0]);
     }
   });
 }
@@ -97,9 +112,11 @@ tinymce.PluginManager.add('menusave', function(editor, url) {
     editor.addMenuItem('menusave', {
         text: 'Save',
         context: 'file',
-        //shortcut: 'Ctrl+s',
+        shortcut: 'Ctrl+s',
         onclick: save
     });
+    editor.addShortcut('Meta+S', '', function(){save();});
+
     editor.addMenuItem('menusaveas', {
         text: 'Save As',
         context: 'file',
@@ -120,27 +137,116 @@ tinymce.init({
   selector:'div.tinymce-full',
   height: "100%",
   indent: true,
-  theme: 'modern',
-  extended_valid_elements : "link[rel|href],a[class|name|href|target|title|onclick|rel],script[type|src],iframe[src|style|width|height|scrolling|marginwidth|marginheight|frameborder],img[class|src|border=0|alt|title|hspace|vspace|width|height|align|onmouseover|onmouseout|name]",
+    theme: 'modern',
+    valid_elements: "+*[*]", // allow all elements and attributes
+  //#extended_valid_elements : "link[rel|href],a[class|name|href|target|title|onclick|rel],script[type|src],iframe[src|style|width|height|scrolling|marginwidth|marginheight|frameborder],img[class|src|border=0|alt|title|hspace|vspace|width|height|align|onmouseover|onmouseout|name]",
   plugins: [
     'advlist autolink lists link image charmap print hr anchor pagebreak',
-    'searchreplace wordcount visualblocks visualchars code fullscreen',
-    'insertdatetime media nonbreaking save table contextmenu directionality',
-    'emoticons template paste textcolor colorpicker textpattern imagetools codesample toc',
+    'searchreplace wordcount visualblocks visualchars code importcss', //  autosave
+    'insertdatetime media nonbreaking table contextmenu directionality',
+    'emoticons template paste textcolor colorpicker textpattern imagetools toc', //codesample
     'menusave',
-    'fullpage'
+      'fullpage',
   ],
-  file_picker_types: 'image', 
-  // and here's our custom image picker
+  toolbar: 'undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | numlist bullist | outdent indent | code',
+
+  //menubar: "tools",
+  element_format: "xhtml",
+  fix_list_elements: true,
+  file_picker_types: 'image',
+  entities: '160,#160', // # use &#160; instead of &nbsp;
+  content_css:  "nde-ed.css",
+  importcss_append :  true,
+  importcss_groups: [
+      {title: "Figures", filter: /^(figure|figcaption)\./},
+      {title: "Sidebars", filter: /^aside\./},
+      {title: "Frames", filter: /.*frame./},
+  ],
+    
+    importcss_selector_filter: function(selector) {
+	regex=/^[-_0-9a-zA-Z]+[.][-_0-9a-zA-Z]+$/; // only allow tag name + one class
+	return selector.match(regex) && selector != "div.nde-ed-edobjective" && selector != "div.nde-ed-review" && selector != "div.displaymath";
+	
+    }, 
+  visualblocks_content_css:  new URL("node_modules/tinymce/plugins/visualblocks/css/visualblocks.css", document.baseURI).href,
+    image_caption: true,  // enable image captioning
+    image_advtab: true,
+    //style_formats: [
+    //  {title: "figure", block: "figure", wrapper: true},
+    //  {title: "figcaption", block: "figcaption", wrapper: true},
+  //],
+  //  valid_children: '+figure[img],+figure[figcaption]',
+    // and here's our custom image picker
+    style_formats: [
+	{ title: 'Create sidebar; then style below', block: 'aside', wrapper: true },
+	{ title: 'Educational objective; add h3 heading then add bulleted list', block: 'div', wrapper: true, 
+	  classes: "nde-ed-edobjective" },
+	{ title: 'Review; add h3 heading then add numbered list', block: 'div', wrapper: true, 
+	  classes: "nde-ed-review" },
+	{ title: 'DisplayMath', block: 'div', wrapper: true, 
+	  classes: "displaymath" }
+    ],
+    style_formats_merge: true,
   file_picker_callback: function(cb, value, meta) {
     dialog.showOpenDialog({
       properties: ["openFile"],
       filters: [{name: 'Images', extensions: ['jpg', 'png', 'gif', 'svg']}],
       defaultPath: working_directory
-    }, function(fn) {
-      var filename = path.basename(fn[0]);
-      var rel_filename = path.relative(working_directory || "", fn[0]);
+    }).then((fn) => {
+      if (fn.canceled) return;
+      var filename = path.basename(fn.filePaths[0]);
+      var rel_filename = path.relative(working_directory || "", fn.filePaths[0]);
       cb(rel_filename, { title: filename});
     });
+  },
+  setup: function(editor) {
+      editor.on('init', function(e) {
+	  //console.log("argv is:");
+	  //console.log(require('electron').remote.process.argv);
+	  var argv_array = require('electron').remote.process.argv;
+	  if (argv_array.length > 2) {
+	      console.log("argv[2] is:");
+	      console.log(argv_array[2]);
+	      // load file specified on command line
+
+	      
+	      //filename=path.basename(argv_array[2]);
+	      var filepath=path.resolve(argv_array[2]);
+	      filename = path.basename(filepath); // set global
+	      change_working_directory(path.dirname(filepath));
+	      console.log('calling readFile()');
+	      console.log(filepath);
+	      fs.readFile(filepath, 'utf8', (err, data) => {
+		  if (err) throw err;
+		  editor.setContent(data);
+	      });
+	  
+	  } else {
+	      // initialize with blank document 
+	      editor.setContent(`<?xml version="1.0" encoding="utf-8"?>
+<?xml-stylesheet type="text/xsl" href="topic.xsl"?>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>What is Electricity?</title>
+<!-- <meta name="keywords" content=""/> --> <!-- keywords separated by commas and spaces -->
+<!-- <meta name="description" content=""/> -->
+<!-- <meta name="author" content=""/>  -->
+</head>
+<body>
+<p>
+Enter content here.
+</p><p>Try to use relative sizing for  
+images (use percentage under Dimensions and Check "constrain proportions")
+</p><p>  
+Be sure to set the metadata (under File... Document Properties). In 
+particular the title must match exactly either the navigation 
+name or navigation shortname for the page.  
+</p>
+</body>
+</html>
+`);
+	  }
+      });
   }
-});
+}	     // end of init() call...	     
+	    );
